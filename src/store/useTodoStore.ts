522552f6024f99860e35c7b68ba0cf2f4be66fe7
Dondon0313 <1234eggs@gmail.com>
2025-03-24 17:33:09 +0800

    使用 localStorage 存資料，並在初始化時載入。

--- src/store/useTodoStore.ts
+++ src/store/useTodoStore.ts
@@ -1,9 +1,21 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import type { Todo } from '@/types/Todo'
 
+const LOCAL_STORAGE_KEY = 'todo-list'
+
 export const useTodoStore = defineStore('todo', () => {
-  const todos = ref<Todo[]>([])
+
+  const saved = localStorage.getItem(LOCAL_STORAGE_KEY)
+  const todos = ref<Todo[]>(saved ? JSON.parse(saved) : [])
+
+  watch (
+    todos,
+    (newTodos) => {
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newTodos))
+    },
+    { deep: true},
+  )
 
   function addTodo(text: string) {
     todos.value.push({ id: Date.now(), text, completed: false })
